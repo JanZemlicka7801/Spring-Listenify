b234bb26eedc1ce68e36accbc2d8731ee95c4cd9
Malikom12 <muhammadmalikomer@gmail.com>
2025-01-06 03:00:41 +0000

    Implemented AlbumController with new get all albums method

--- src/main/java/listenify/controllers/AlbumController.java
+++ src/main/java/listenify/controllers/AlbumController.java
@@ -1,4 +1,28 @@
 package listenify.controllers;
 
+import listenify.business.Albums;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import listenify.persistence.AlbumsDao;
+import listenify.persistence.AlbumDaoImpl;
+
+import java.util.List;
+
+@Controller
 public class AlbumController {
+
+    private final AlbumsDao albumsDao;
+
+    public AlbumController(){
+        this.albumsDao = new AlbumDaoImpl("database.properties");
+    }
+
+    @GetMapping("/viewAlbums")
+    public String processRequest(Model model) {
+        List<Albums> albums = albumsDao.getAllAlbums();
+        model.addAttribute("albums", albums);
+        return "albums";
+    }
+
 }
