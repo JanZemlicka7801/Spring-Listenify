3249bdd92947c6cc56fafecf9815ee82616f6519
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-01-06 14:12:54 +0000

    Added a display of the most popular song in the main page.

--- src/main/java/listenify/controllers/IndexController.java
+++ src/main/java/listenify/controllers/IndexController.java
@@ -1,13 +1,29 @@
 package listenify.controllers;
 
+import listenify.persistence.RatingDaoImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
 
+    private final RatingDaoImpl ratingDao;
+
+    public IndexController(){
+        this.ratingDao = new RatingDaoImpl("database.properties");
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String homePage(Model model) {
+        String mostPopularSong;
+        try {
+            mostPopularSong = ratingDao.getTheMostPopularSong();
+        } catch (Exception e) {
+            mostPopularSong = "Unable to fetch the most popular song.";
+            e.printStackTrace();
+        }
+        model.addAttribute("mostPopularSong", mostPopularSong);
         return "index";
     }
 
